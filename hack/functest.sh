d7bd235053e6517213552285b0335631c3990991
Radim Hrazdil <rhrazdil@redhat.com>
2020-09-16 16:26:32 +0200

    Fix test verbose flag setting
    Signed-off-by: Radim Hrazdil <rhrazdil@redhat.com>

--- hack/functest.sh
+++ hack/functest.sh
@@ -4,4 +4,4 @@ set -ex
 
 source ./cluster/kubevirtci.sh
 
-go test ./tests/e2e --kubeconfig $(kubevirtci::kubeconfig) -v --ginkgo.v
+go test ./tests/e2e --kubeconfig $(kubevirtci::kubeconfig) -ginkgo.v
