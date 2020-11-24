3c2cd0eaee7659bf15e8fe9a703864634f75e1a5
Ram Lavi <ralavi@redhat.com>
2020-11-24 09:42:31 +0200

    cluster scripts, Fix cluster.sh to remove _kubevirtci folder
    Currently, when kubevirtci tag version is changed, we get
    an error when accessing kubevirtci::install.
    Added a fix that will clean up the old _kubevirtci folder
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- cluster/cluster.sh
+++ cluster/cluster.sh
@@ -3,10 +3,27 @@
 
 export KUBEVIRT_PROVIDER="${KUBEVIRT_PROVIDER:-k8s-1.19}"
 export KUBEVIRTCI_TAG="2011240648-fb5563f"
+
+KUBEVIRTCI_REPO='https://github.com/kubevirt/kubevirtci.git'
 # The CLUSTER_PATH var is used in cluster folder and points to the _kubevirtci where the cluster is deployed from.
 CLUSTER_PATH=${CLUSTER_PATH:-"${PWD}/_kubevirtci/"}
 
+function cluster::_get_repo() {
+    git --git-dir ${CLUSTER_PATH}/.git remote get-url origin
+}
+
+function cluster::_get_tag() {
+    git --git-dir ${CLUSTER_PATH}/.git describe --tags
+}
+
 function cluster::install() {
+    # Remove cloned kubevirtci repository if it does not match the requested one
+    if [ -d ${CLUSTER_PATH} ]; then
+        if [ $(cluster::_get_repo) != ${KUBEVIRTCI_REPO} -o $(cluster::_get_tag) != ${KUBEVIRTCI_TAG} ]; then
+            rm -rf ${CLUSTER_PATH}
+        fi
+    fi
+
     if [ ! -d ${CLUSTER_PATH} ]; then
         git clone https://github.com/kubevirt/kubevirtci.git ${CLUSTER_PATH}
         (
