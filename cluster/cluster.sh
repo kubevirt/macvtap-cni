98521f26ff31ac308a65426250443c4217b46a37
Miguel Duarte Barroso <mdbarroso@redhat.com>
2023-02-28 10:01:05 +0100

    ci: bump to kubevirtci k8s-1.24
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- cluster/cluster.sh
+++ cluster/cluster.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export KUBEVIRT_PROVIDER="${KUBEVIRT_PROVIDER:-k8s-1.23}"
+export KUBEVIRT_PROVIDER="${KUBEVIRT_PROVIDER:-k8s-1.24}"
 export KUBEVIRTCI_TAG=$(curl -L -Ss https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirtci/latest)
 export KUBEVIRTCI_RUNTIME=${OCI_BIN:-docker}
 
