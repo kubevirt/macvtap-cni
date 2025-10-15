50ac4957dad7c947588259e2d6f3dfc15100f285
Or Shoval <oshoval@redhat.com>
2025-10-15 15:39:28 +0300

    Use Flannel
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- cluster/cluster.sh
+++ cluster/cluster.sh
@@ -3,6 +3,7 @@
 export KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER:-k8s-1.33}
 export KUBEVIRTCI_TAG=${KUBEVIRTCI_TAG:-2509181951-8264c60a}
 export KUBEVIRTCI_RUNTIME=${OCI_BIN:-docker}
+export KUBEVIRT_FLANNEL=true
 
 KUBEVIRTCI_REPO='https://github.com/kubevirt/kubevirtci.git'
 # The CLUSTER_PATH var is used in cluster folder and points to the _kubevirtci where the cluster is deployed from.
